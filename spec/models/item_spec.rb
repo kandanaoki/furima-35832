63ba9a1550df1e62b71ff7ef315c5444108311c8
kandanaoki <84160141+kandanaoki@users.noreply.github.com>
2021-06-08 14:43:46 +0900

    商品一覧表示の実装

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -93,7 +93,7 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
       end
       it '価格は、¥300~¥9,999,999の間のみ保存可能であること。(9,999,999以上Ver)' do
-        @item.price = 10000000
+        @item.price = 10_000_000
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
